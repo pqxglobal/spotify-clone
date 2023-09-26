fb278dd07cb696ecbace9a086871fbb54e679f9f
pqxglobal <l.r.slegt@gmail.com>
2023-09-26 10:03:25 +0200

    🚧 2:02:34

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+// Packages
+import { useEffect, useState } from 'react'
+
+// Custom Imports
+import Modal from '@/components/Modal'
+
+const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted) {
+    return null
+  }
+
+  return (
+    <>
+      <Modal
+        title="Test Modal"
+        description="Test Description"
+        isOpen
+        onChange={() => {}}
+      >
+        Test Children
+      </Modal>
+    </>
+  )
+}
+export default ModalProvider
