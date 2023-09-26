a5631483750c781b1fe2328ccc7c021b47986da0
pqxglobal <l.r.slegt@gmail.com>
2023-09-26 04:39:43 +0200

    🚧 1:36:35

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Sidebar from '@/components/Sidebar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
+import SupabaseProvider from '@/providers/SupabaseProvider'
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -10,14 +11,14 @@ export const metadata: Metadata = {
   description: 'Listen to music!',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={font.className}><Sidebar>{children}</Sidebar></body>
+      <body className={font.className}>
+        <SupabaseProvider>
+          <Sidebar>{children}</Sidebar>
+        </SupabaseProvider>
+      </body>
     </html>
   )
 }
