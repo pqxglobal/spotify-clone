0fe9a2be7405f2c368ae09d7995881d7ef601010
pqxglobal <l.r.slegt@gmail.com>
2023-09-27 03:11:07 +0200

    🚧 2:41:21

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react'
 
 // Custom Imports
 import AuthModal from '@/components/AuthModal'
+import UploadModal from '@/components/UploadModal'
 
 const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false)
@@ -20,6 +21,7 @@ const ModalProvider = () => {
   return (
     <>
       <AuthModal />
+      <UploadModal />
     </>
   )
 }
