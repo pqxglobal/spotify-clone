a36566bb8f4f6012d3a6f1536f01afd486187c3a
pqxglobal <l.r.slegt@gmail.com>
2023-09-29 08:57:38 +0200

    🚧 4:11:37

---
+++ components/Player.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import useGetSongById from '@/hooks/useGetSongById'
+import usePlayer from '@/hooks/usePlayer'
+
+const Player = () => {
+  const player = usePlayer()
+  const { song } = useGetSongById(player.activeId)
+
+  return <div>Player!</div>
+}
+export default Player
