5e1be307992345f9fdd414273e495d9f346fe457
pqxglobal <l.r.slegt@gmail.com>
2023-09-29 12:28:00 +0200

    🚧 4:32:00

--- components/Player.tsx
+++ components/Player.tsx
@@ -4,6 +4,8 @@ import useGetSongById from '@/hooks/useGetSongById'
 import useLoadSongUrl from '@/hooks/useLoadSongUrl'
 import usePlayer from '@/hooks/usePlayer'
 
+import PlayerContent from './PlayerContent'
+
 const Player = () => {
   const player = usePlayer()
   const { song } = useGetSongById(player.activeId)
@@ -26,7 +28,11 @@ const Player = () => {
         px-4
       "
     >
-      Player!
+      <PlayerContent
+        key={songUrl}
+        song={song}
+        songUrl={songUrl}
+      />
     </div>
   )
 }
